d090050ff077ddac7aa25c4cb3ef00a2d061a004
Roman Dryndik <dryndikroman@gmail.com>
2022-11-19 15:24:14 +0100

    Finalized session.

--- session.py
+++ session.py
@@ -1,4 +1,6 @@
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = None
+
+engine = create_engine("mysql+pymysql://root:root@localhost:3306/bwitter")
 Session = sessionmaker(bind=engine)
